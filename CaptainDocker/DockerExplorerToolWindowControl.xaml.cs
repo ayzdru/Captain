f931e3f8aafa309e5fdf7d27a842ce91f8809a5b
Ayaz Duru <ayzdru@outlook.com>
2020-02-20 18:30:12 +0300

    Proje oluşturuldu.

---
+++ CaptainDocker/DockerExplorerToolWindowControl.xaml.cs
@@ -0,0 +1,54 @@
+﻿namespace CaptainDocker
+{
+    using CaptainDocker.ValueObjects;
+    using Docker.DotNet;
+    using Docker.DotNet.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for DockerExplorerToolWindowControl.
+    /// </summary>
+    public partial class DockerExplorerToolWindowControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DockerExplorerToolWindowControl"/> class.
+        /// </summary>
+        public DockerExplorerToolWindowControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void RefreshButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            dockerExplorerTreeView.Items.Clear();
+            DockerClient dockerClient = new DockerClientConfiguration(
+ new Uri("http://kubernetemaster:4243/"))
+ .CreateClient();
+            IList<ContainerListResponse> containers =await dockerClient.Containers.ListContainersAsync(
+      new ContainersListParameters()
+      {
+          Limit = 10,
+      });
+
+            DockerTreeViewItem root = new DockerTreeViewItem() { Title = dockerClient.Configuration.EndpointBaseUri.Host };
+            DockerTreeViewItem childItem1 = new DockerTreeViewItem() { Title = "Containers" };
+            foreach (var container in containers)
+            {
+                childItem1.Items.Add(new DockerTreeViewItem() { Title = string.Join(",",container.Names) });
+            }
+            root.Items.Add(childItem1);
+            var images = await dockerClient.Images.ListImagesAsync(new ImagesListParameters() { All = true }); 
+            DockerTreeViewItem childItem2 = new DockerTreeViewItem() { Title = "Images" };
+            foreach (var image in images)
+            {
+                childItem2.Items.Add(new DockerTreeViewItem() { Title = image.ID });
+            }
+            root.Items.Add(childItem2);
+            dockerExplorerTreeView.Items.Add(root);
+        }
+    }
+}
---
+++ CaptainDocker/ValueObjects/DockerTreeViewItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptainDocker.ValueObjects
+{
+    public class DockerTreeViewItem
+    {
+        public DockerTreeViewItem()
+        {
+            this.Items = new ObservableCollection<DockerTreeViewItem>();
+        }
+        public string Title { get; set; }
+        public ObservableCollection<DockerTreeViewItem> Items { get; set; }
+    }
+}
